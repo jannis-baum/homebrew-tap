7f722504c85374b8def3ca72f9db0352c0f665f0
Jannis Baum <jannis.baum@gmail.com>
2024-07-19 16:26:18 +0200

    chore: update vivify 0.2.0

--- Formula/vivify.rb
+++ Formula/vivify.rb
@@ -1,27 +1,21 @@
 class Vivify < Formula
   desc "Bring your Markdown to life"
   homepage "https://github.com/jannis-baum/Vivify"
-  version "0.1.6"
+  version "0.2.0"
   license "GPL-3.0"
 
-  depends_on "jq"
-
   on_macos do
-    url "https://github.com/jannis-baum/Vivify/releases/download/v0.1.6/vivify-macos.zip"
-    sha256 "536a82da191e66f773dc88643a241639ce1765e8f015921858515f988cfb34c7"
+    url "https://github.com/jannis-baum/Vivify/releases/download/v0.2.0/vivify-macos.tar.gz"
+    sha256 "75003b0399b81cb2010240b40a0d4b59c9a01cc387c42a1e562142ad668743f3"
 
-    def install
-      bin.install "macos/vivify-server"
-      bin.install "macos/viv"
-    end
   end
   on_linux do
-    url "https://github.com/jannis-baum/Vivify/releases/download/v0.1.6/vivify-linux.zip"
-    sha256 "7a4997224aaca0946bb2ef217a865a55014bb34d874b851f1b848758e4f35422"
+    url "https://github.com/jannis-baum/Vivify/releases/download/v0.2.0/vivify-linux.tar.gz"
+    sha256 "91d2935d67c4ba2f2fbed3cace72df35a62a1b4ca40a5e75e1fbbd4e43563931"
+  end
 
-    def install
-      bin.install "linux/vivify-server"
-      bin.install "linux/viv"
-    end
+  def install
+    bin.install "vivify-server"
+    bin.install "viv"
   end
 end
