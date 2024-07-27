dc569710192816f9b728f8639ca86092fa2dbe96
Jannis Baum <jannis.baum@gmail.com>
2024-07-27 08:33:26 +0200

    refactor: version interpolation & test

--- Formula/vivify.rb
+++ Formula/vivify.rb
@@ -5,17 +5,21 @@ class Vivify < Formula
   license "GPL-3.0"
 
   on_macos do
-    url "https://github.com/jannis-baum/Vivify/releases/download/v0.2.1/vivify-macos.tar.gz"
+    url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-macos.tar.gz"
     sha256 "509fc47284d76eec357bb9b0268aee7df987f714e0137bafe576517a606c38e2"
 
   end
   on_linux do
-    url "https://github.com/jannis-baum/Vivify/releases/download/v0.2.1/vivify-linux.tar.gz"
+    url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-linux.tar.gz"
     sha256 "c2feb1f1c1f3dcbce510268bb2a9f4dae626d7351e47c95325c9c9b2c5af1c14"
   end
 
   def install
     bin.install "vivify-server"
     bin.install "viv"
+  end
+
+  test do
+    system "#{bin}/viv", "--version"
   end
 end
