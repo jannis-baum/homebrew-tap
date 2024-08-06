c8f21d09a50b302f598e500240b8faa15015ec35
Jannis Baum <jannis.baum@gmail.com>
2024-08-06 16:07:55 +0200

    chore: vivify 0.3.1

--- Formula/vivify.rb
+++ Formula/vivify.rb
@@ -1,17 +1,17 @@
 class Vivify < Formula
   desc "Bring your Markdown to life"
   homepage "https://github.com/jannis-baum/Vivify"
-  version "0.3.0"
+  version "0.3.1"
   license "GPL-3.0"
 
   on_macos do
     url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-macos.tar.gz"
-    sha256 "24617d3cbaca4fd0cd5875906b3796a13664b424e502cce8e72a1d9750c55e5f"
+    sha256 "179e83c0255b736bfc8ed1a9f21900d16631e99fa7d63fa22c95e250c1edbb52"
 
   end
   on_linux do
     url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-linux.tar.gz"
-    sha256 "65d73ab650670a07f5a77f004df0749150b32d5a8aa842955e10b60ba021babc"
+    sha256 "e7ac880756d9d21ef81f75a60f4ce4489a6d3c5ea1df40cf8233b437f4bc2e72"
   end
 
   def install
