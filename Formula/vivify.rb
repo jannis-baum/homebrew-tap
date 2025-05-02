c0d9d5ad97a46dcbfffe4f647f1dceaad2f6d420
Jannis Baum <jannis.baum@gmail.com>
2025-05-02 18:04:51 +0200

    chore: vivify 0.8.0

--- Formula/vivify.rb
+++ Formula/vivify.rb
@@ -1,25 +1,25 @@
 class Vivify < Formula
   desc "Bring your Markdown to life"
   homepage "https://github.com/jannis-baum/Vivify"
-  version "0.7.0"
+  version "0.8.0"
   license "GPL-3.0"
 
   # node dependencies, see https://formulae.brew.sh/formula/node
   depends_on "brotli"
   depends_on "c-ares"
-  depends_on "icu4c@76"
+  depends_on "icu4c@77"
   depends_on "libnghttp2"
   depends_on "libuv"
   depends_on "openssl@3"
 
   on_macos do
     url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-macos.tar.gz"
-    sha256 "a12544d05eb40537b2784233f7a341e1b5c063d20911fb97e574b0e46b89299c"
+    sha256 "58258904808fecddea112b51da0ce1d774e4252ca4752c3767be8f2e5dd7d34d"
   end
 
   on_linux do
     url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-linux.tar.gz"
-    sha256 "88081c6846f675c55e92e7acd0ab0d9b410b2adc4030d3269e542efa3e4e7514"
+    sha256 "4c7ff4f1d6dee80bb87242b58ed4302a4b5ef5feb61dce9f80e84127a21377d0"
   end
 
   def install
