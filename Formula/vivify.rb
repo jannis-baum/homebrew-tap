7aba937f3d9690a2fe9f444e9935fd5794e4ceff
Jannis Baum <jannis.baum@gmail.com>
2025-10-02 07:14:41 +0200

    fix: vivify version brew increment

--- Formula/vivify.rb
+++ Formula/vivify.rb
@@ -21,7 +21,7 @@ class Vivify < Formula
       depends_on "uvwasi"
       depends_on "zstd"
       depends_on "simdutf"
-      url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-macos-arm.tar.gz"
+      url "https://github.com/jannis-baum/Vivify/releases/download/v0.10.0/vivify-macos-arm.tar.gz"
       sha256 "69c6f2c51568db7bd658ef23974fca969601b7d793d917a246d9256fa8a7c192"
     end
     on_intel do
@@ -35,7 +35,7 @@ class Vivify < Formula
       depends_on "openssl@3"
       depends_on "sqlite"
       depends_on "zstd"
-      url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-macos-x86.tar.gz"
+      url "https://github.com/jannis-baum/Vivify/releases/download/v0.10.0/vivify-macos-x86.tar.gz"
       sha256 "03a54f9fb260c128b5d9aebad4df54b8b4d3a01043809765dd8d6202cd680635"
     end
   end
@@ -53,7 +53,7 @@ class Vivify < Formula
     depends_on "sqlite"
     depends_on "uvwasi"
     depends_on "zstd"
-    url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-linux.tar.gz"
+    url "https://github.com/jannis-baum/Vivify/releases/download/v0.10.0/vivify-linux.tar.gz"
     sha256 "f24b542dcf7bdc0333dba44745e7570fff058e775eed633287eeb54d87df7121"
   end
 
