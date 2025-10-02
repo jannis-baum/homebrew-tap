474271fd82ff4e0a43421e780fb0eaf4f7beaf78
Jannis Baum <jannis.baum@gmail.com>
2025-10-02 08:02:27 +0200

    chore: build vivify locally

--- Formula/vivify.rb
+++ Formula/vivify.rb
@@ -1,64 +1,19 @@
 class Vivify < Formula
-  desc "Bring your Markdown to life"
+  desc "Live viewer for Markdown, HTML, Jupyter Notebooks & more"
   homepage "https://github.com/jannis-baum/Vivify"
-  version "0.10.0-1"
+  version "0.10.0"
   license "GPL-3.0"
 
-  # dependencies are from node, see https://formulae.brew.sh/formula/node
+  depends_on "node"
+  depends_on "yarn"
 
-  on_macos do
-    on_arm do
-      depends_on "brotli"
-      depends_on "c-ares"
-      depends_on "icu4c@77"
-      depends_on "libnghttp2"
-      depends_on "libnghttp3"
-      depends_on "libngtcp2"
-      depends_on "libuv"
-      depends_on "openssl@3"
-      depends_on "simdjson"
-      depends_on "sqlite"
-      depends_on "uvwasi"
-      depends_on "zstd"
-      depends_on "simdutf"
-      url "https://github.com/jannis-baum/Vivify/releases/download/v0.10.0/vivify-macos-arm.tar.gz"
-      sha256 "69c6f2c51568db7bd658ef23974fca969601b7d793d917a246d9256fa8a7c192"
-    end
-    on_intel do
-      depends_on "brotli"
-      depends_on "c-ares"
-      depends_on "icu4c@77"
-      depends_on "libnghttp2"
-      depends_on "libnghttp3"
-      depends_on "libngtcp2"
-      depends_on "libuv"
-      depends_on "openssl@3"
-      depends_on "sqlite"
-      depends_on "zstd"
-      url "https://github.com/jannis-baum/Vivify/releases/download/v0.10.0/vivify-macos-x86.tar.gz"
-      sha256 "03a54f9fb260c128b5d9aebad4df54b8b4d3a01043809765dd8d6202cd680635"
-    end
-  end
-
-  on_linux do
-    depends_on "brotli"
-    depends_on "c-ares"
-    depends_on "icu4c@77"
-    depends_on "libnghttp2"
-    depends_on "libnghttp3"
-    depends_on "libngtcp2"
-    depends_on "libuv"
-    depends_on "openssl@3"
-    depends_on "simdjson"
-    depends_on "sqlite"
-    depends_on "uvwasi"
-    depends_on "zstd"
-    url "https://github.com/jannis-baum/Vivify/releases/download/v0.10.0/vivify-linux.tar.gz"
-    sha256 "f24b542dcf7bdc0333dba44745e7570fff058e775eed633287eeb54d87df7121"
-  end
+  url "https://github.com/jannis-baum/Vivify/archive/refs/tags/v#{version}.zip"
+  sha256 "3d9ad60cbd30d2dd85726ea9bac9c62987e4477c38355de44bf0e21830bfdb6d"
 
   def install
-    bin.install "vivify-server"
-    bin.install "viv"
+    bin.mkpath
+    system "yarn"
+    system "./configure", bin
+    system "make", "install"
   end
 end
