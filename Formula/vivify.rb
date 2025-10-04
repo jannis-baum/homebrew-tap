bd33388f0045c2be7a99085f0394a7913261b420
Jannis Baum <jannis.baum@gmail.com>
2025-10-04 08:57:16 +0200

    chore: vivify 0.10.1

--- Formula/vivify.rb
+++ Formula/vivify.rb
@@ -1,19 +1,27 @@
 class Vivify < Formula
   desc "Live viewer for Markdown, HTML, Jupyter Notebooks & more"
   homepage "https://github.com/jannis-baum/Vivify"
-  version "0.10.0"
+  version "0.10.1"
   license "GPL-3.0"
 
-  depends_on "node"
-  depends_on "yarn"
+  on_macos do
+    on_arm do
+      url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-macos-arm.tar.gz"
+      sha256 "470a1f7061472517dad886c88bd5b0963449f2c6d4e85ce3f01d5d2dfc1c4571"
+    end
+    on_intel do
+      url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-macos-x86.tar.gz"
+      sha256 "03ec463358ee9d108298a72df226c8d8f07c835184c17511ae339c3e15b40b71"
+    end
+  end
 
-  url "https://github.com/jannis-baum/Vivify/archive/refs/tags/v#{version}.zip"
-  sha256 "3d9ad60cbd30d2dd85726ea9bac9c62987e4477c38355de44bf0e21830bfdb6d"
+  on_linux do
+    url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-linux.tar.gz"
+    sha256 "349e17de3646368bd69c47282bba66b2812c188584a6a1597a65ce3f6b576a42"
+  end
 
   def install
-    bin.mkpath
-    system "yarn"
-    system "./configure", bin
-    system "make", "install"
+    bin.install "vivify-server"
+    bin.install "viv"
   end
 end
