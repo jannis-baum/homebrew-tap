481ab28f344c8c7a3665b3fcd159345249d2cbe9
Jannis Baum <plant.lull_1o@icloud.com>
2026-01-09 07:43:27 +0100

    chore: vivify 0.12.0

--- Formula/vivify.rb
+++ Formula/vivify.rb
@@ -1,23 +1,23 @@
 class Vivify < Formula
   desc "Live viewer for Markdown, HTML, Jupyter Notebooks & more"
   homepage "https://github.com/jannis-baum/Vivify"
-  version "0.11.0"
+  version "0.12.0"
   license "GPL-3.0"
 
   on_macos do
     on_arm do
       url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-macos-arm.tar.gz"
-      sha256 "61bad94726abe4490c1ca3cadff4df80faa6cfa6cb15e63e20f6b077c144ed48"
+      sha256 "08f99b30cd26503983eaf0bec88ba09d23bc88e31e84ff1cac4f32adf06737f8"
     end
     on_intel do
       url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-macos-x86.tar.gz"
-      sha256 "88b55517b28950be7184e0d0429ccad5f7268f83071f9fd48324a0ed22adae81"
+      sha256 "cd2e41e32695b9ad558ee6e9b5701271b7812f349f4b7968f1bbd367cac09466"
     end
   end
 
   on_linux do
     url "https://github.com/jannis-baum/Vivify/releases/download/v#{version}/vivify-linux.tar.gz"
-    sha256 "a961efc65006bc1cb30842076c284cd748e79c6592d2c1e2046e8aba1815efa1"
+    sha256 "b655987f38177720b88b908bd9748425e26e42cafd368df0e06b2f17e3229267"
   end
 
   def install
